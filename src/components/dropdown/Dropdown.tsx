96b26b5bdd222a68e64d41e42c2f4eec135082d7
Artem Ibragimov <aribragimov@inbox.ru>
2023-09-23 20:17:43 +0300

    refactor code

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,45 +1,41 @@
-import {FC, SVGProps, useState} from "react";
+import {FC, SVGProps, useRef, useState} from "react";
 import s from './Dropdown.module.css';
-
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 interface Dropdown {
     options: string[];
     Icon: FC<SVGProps<SVGAElement>>;
     ActiveIcon: FC<SVGProps<SVGAElement>>;
-    handleClick: (arg0: string) => void;
+    handleClick: (name: string) => void;
     isActive: (name: string) => boolean;
 }
 
 const Dropdown = ({options, Icon, ActiveIcon, handleClick, isActive}: Dropdown) => {
 
     const [selected, setSelected] = useState<string>('All');
-    const [dropdown, setDropdown] = useState<boolean | null>(null);
+
+    const [dropdown, setDropdown] = useState<boolean>(false);
+    const divRef = useRef<HTMLUListElement>(null);
+    useOnClickOutside(divRef, () => setDropdown(false));
 
     const isActivated = isActive(selected);
 
     const handleItemClick = (name: string) => {
         handleClick(name);
         setSelected(name);
-        setDropdown(!dropdown);
-    };
-
-    const handleMouseOver = () => {
-        setDropdown(true);
-    };
-
-    const handleMouseOut = () => {
         setDropdown(false);
     };
 
     return (
-        <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-            <button type="button" className={s.btn + ' ' + `${isActivated ? s.btn_active : s.btn_nonActive}`}>
+        <div>
+            <button onClick={() => setDropdown(true)} type="button"
+                    className={s.btn + ' ' + `${isActivated ? s.btn_active : s.btn_nonActive}`}>
                 {isActivated ? <ActiveIcon/> : <Icon/>}
                 {selected}
             </button>
 
             {dropdown &&
-                <ul className={s.list}>
+                <ul ref={divRef} className={s.list}>
                     {options.map((option: string, i: number) => (
                         <li key={i}
                             className={s.btn + ' ' + s.item + ' ' + `${isActive(option) ? s.btn_active : s.btn_nonActive}`}
